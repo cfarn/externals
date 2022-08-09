b746cc29b97446a18896492af7c7023affecbd42
cfarn <cinniefarnaud@gmail.com>
2022-08-09 15:34:58 +0200

    correction

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,24 +2,18 @@ const express = require('express')
 const app = express()
 const axios = require("axios")
 
-// app.get('/url', async(req, res) => {
-//     const response = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-//     res.json(response.data)
-// })
-
-// app.get('/url', async(req, res) => {
-//     const response = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=1")
-//     res.json(response.data)
-// })
-
+app.get('/url', async(req, res) => {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+    res.json(response.data.results)
+})
 
 app.get('/url/:id', async(req, res) => {
     const {id} = req.params
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?${id}`)
-    if({id}){
+    try{
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?${id}`)
         res.json(response.data)
-    }else {
-        res.status(404).send('Not Found')
+    }catch(error){
+        res.status(error.response.status).send(error.response.statusText)
     }
 })
 
